c8c798a090ae786a5a6e5d2365946bd1b851895f
Val_F4 <vallkemgamer@gmail.com>
2024-02-17 22:43:12 -0300

    Create Open Gui system and CLosed Gui system with background animation.

--- src/ReplicatedStorage/ClientServices/GuiControl/init.lua
+++ src/ReplicatedStorage/ClientServices/GuiControl/init.lua
@@ -1,4 +1,8 @@
 local ReplicatedStore = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
+
+local timeInitAnimation: TweenInfo = TweenInfo.new(.2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut)
+local InifinitTimeAnimation: TweenInfo = TweenInfo.new(60, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 1, true)
 
 local EffectControler = require(script.GuiEffects)
 
@@ -6,13 +10,36 @@ local InGui: string = nil
 
 local GuiControl = {}
 
-local function ChargedGui(): ()
-	return
+local function OpenGui(Player: Player, Gui: string, CodePosition: UDim2): ()
+	if not Player or not Gui then return end
+
+	local PlayerGui: PlayerGui = Player.PlayerGui
+	local Game_Guis: Folder = PlayerGui:FindFirstChild("GAME_GUIS")
+	local GuiManager: ScreenGui = Game_Guis:FindFirstChild(Gui)
+
+	if not GuiManager then return end
+
+	local GetBackgroundEffect: ImageLabel = GuiManager:FindFirstChild("Texture")
+	local GetMainFrame: Frame = GuiManager:FindFirstChild("MainFrame")
+	if not GetBackgroundEffect or not GetMainFrame then return end
+
+	local OpenFrameAnimation: Tween = TweenService:Create(GetMainFrame, timeInitAnimation, {Position = UDim2.new(CodePosition.X, CodePosition.Y)}) -- {0.173, 0},{-5, 0}
+	local InitBackgroundAnimation: Tween = TweenService:Create(GetBackgroundEffect, timeInitAnimation, {ImageTransparency = 0.6})
+	local infinitAnimationBackground: Tween = TweenService:Create(GetBackgroundEffect, InifinitTimeAnimation, {Position = UDim2.fromScale(1.0, 0.5)})
+
+	GuiManager.Enabled = true
+
+	OpenFrameAnimation:Play()
+	infinitAnimationBackground:Play()
+	InitBackgroundAnimation:Play()
+
 end
 
 local function HudControler(Player: Player, State: boolean): ()
-	local PlayerGui: PlayerGui = Player.PlayerGui or Player:WaitForChild("PlayerGui")
-	local Hud: ScreenGui = PlayerGui:WaitForChild("Hud",3) or PlayerGui.Hud
+	if not Player or State == nil then return end
+
+	local PlayerGui: PlayerGui = Player.PlayerGui
+	local Hud: ScreenGui = PlayerGui:FindFirstChild("Hud") or PlayerGui.Hud
 
 	if State then
 		Hud.Enabled = false
@@ -21,26 +48,44 @@ local function HudControler(Player: Player, State: boolean): ()
 	end
 end
 
-local function RemoveGui(): ()
-	InGui = nil
-	return
+local function RemoveGui(Player: Player ,FindGui: ScreenGui): ()
+	local PlayerGui: PlayerGui = Player.PlayerGui
+	local GameGuiFolder: Folder = PlayerGui:FindFirstChild("GAME_GUIS")
+	local GetScreemGui: ScreenGui = GameGuiFolder:FindFirstChild(FindGui)
+	
+	if not GetScreemGui then return end
+	
+	local GetFrameInGui: Frame = GetScreemGui:FindFirstChild("MainFrame")
+	local GetClosedBottom: ImageButton = GetFrameInGui:FindFirstChild("ExitBottom")
+
+	local function ClosedFrame(): ()
+		local ClosedGuiAnimation: Tween = TweenService:Create(GetFrameInGui, timeInitAnimation, {Position = UDim2.fromScale(0.5, -0.5)})
+		ClosedGuiAnimation:Play()
+		ClosedGuiAnimation.Completed:Wait()
+		EffectControler.Controler(Player, false)
+		HudControler(Player, false)
+		GetScreemGui.Enabled = false
+		InGui = nil
+		return
+	end
+
+	GetClosedBottom.MouseButton1Down:Connect(ClosedFrame)
 end
 
-local function InitGui(Player: Player, CallGui: string): ()
+local function InitGui(Player: Player, CallGui: string, Position: UDim2): ()
 	if not CallGui then return end
 
-	if InGui and InGui ~= CallGui then ChargedGui() end
-
-	if InGui == CallGui then EffectControler.Controler(Player, false) RemoveGui() return end
-
-	--HudControler(Player, true)
+	OpenGui(Player, CallGui, Position)
+	HudControler(Player, true)
 	EffectControler.Controler(Player, true)
+	RemoveGui(Player, CallGui)
 	InGui = CallGui
+
 end
 
 local HudBottons = {
 	["InventoryBottom"]	= function(Player: Player)
-		InitGui(Player, "InventoryGui")
+		InitGui(Player, "InventoryGui", UDim2.fromScale(0.5, 0.5))
 	end,
 
 	["ConfigBottom"] 	=	function(Player)
@@ -51,8 +96,8 @@ local HudBottons = {
 		 InitGui(Player, "CreditGui")
 	end,
 
-	["CodeBottom"] 		=	function(Player)
-		InitGui(Player, "CodeGui")
+	["CodeBottom"] 		=	function(Player: Player)
+		InitGui(Player, "CodeGui", UDim2.fromScale(0.5, 0.5))
 	end,
 
 	["ShopBottom"] 		=	function(Player)
@@ -61,8 +106,8 @@ local HudBottons = {
 }
 
 function GuiControl:Init(Player: Player): ()
-	local PlayerGui: PlayerGui = Player.PlayerGui or Player:WaitForChild("PlayerGui")
-	local hud: ScreenGui = PlayerGui:WaitForChild("Hud",3) or PlayerGui.Hud
+	local PlayerGui: PlayerGui = Player.PlayerGui
+	local hud: ScreenGui = PlayerGui:WaitForChild("Hud", 5) or PlayerGui.Hud
 
 	local Bottons: Frame = hud.Bottons
 
