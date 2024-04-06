aa90c253c1a52e4a95cab2c0758739a8a25ce838
ValF4 <vallkemgamer@gmail.com>
2024-04-06 17:49:11 -0300

    Edit CodeModule

--- src/StarterPlayer/StarterPlayerScripts/localGuiControl.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/localGuiControl.client.lua
@@ -6,10 +6,13 @@ local Bridgnet2 = require(ReplicatedStorage.Packages.BridgeNet2)
 local GuiControl = require(ReplicatedStorage.ClientServices.GuiControl)
 local HudModule = require(PlayerScript.GuisModules.HudModule)
 local UpgradeGui = require(PlayerScript.GuisModules.UpgradeGui)
+local CodeGuiModule = require(PlayerScript.GuisModules.CodeModule)
 
 local exchangeVersion = Bridgnet2.ClientBridge("exchangeVersion")
+--local codeGuiControler = Bridgnet2.ClientBridge("codeGuiControler")
 
 GuiControl:init(Player)
 HudModule:init(Player)
 
 exchangeVersion:Connect(function(Version: string) UpgradeGui.InitGui(Version) end)
+--codeGuiControler:Connect(function() codeGuiControler:init(Player) end)
