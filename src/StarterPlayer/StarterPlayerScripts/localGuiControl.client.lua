cefe57d0dcc2770d551414a24d73be2cc8f198fe
Val_F4 <vallkemgamer@gmail.com>
2024-07-22 23:22:21 -0300

    Create Update progressbar in MissionGui

--- src/StarterPlayer/StarterPlayerScripts/localGuiControl.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/localGuiControl.client.lua
@@ -2,16 +2,19 @@ local Player: Player = game:GetService("Players").LocalPlayer
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local PlayerScript: PlayerScripts = Player.PlayerScripts
 
-local GuiControl        = require(ReplicatedStorage.ClientServices.GuiControl)
-local HudModule         = require(PlayerScript.GuisModules.HudModule)
-local TimeControler     = require(PlayerScript.GuisModules.TimeControler)
-local UpgradeGui        = require(PlayerScript.GuisModules.UpgradeGui)
+local GuiControl            = require(ReplicatedStorage.ClientServices.GuiControl)
+local HudModule             = require(PlayerScript.GuisModules.HudModule)
+local TimeControler         = require(PlayerScript.GuisModules.TimeControler)
+local UpgradeGui            = require(PlayerScript.GuisModules.UpgradeGui)
+local MissionModule         = require(PlayerScript.GuisModules.MissionModule)
 
-local Bridgnet2         = require(ReplicatedStorage.Packages.BridgeNet2)
+local Bridgnet2             = require(ReplicatedStorage.Packages.BridgeNet2)
 
-local exchangeVersion   = Bridgnet2.ClientBridge("exchangeVersion")
+local exchangeVersion       = Bridgnet2.ClientBridge("exchangeVersion")
+local UpgradeProcressBar    = Bridgnet2.ClientBridge("UPGRADE_PROCRESS_BAR")
 
 exchangeVersion:Connect(function(Content: {string}): () UpgradeGui.InitGui(Content) end)
+UpgradeProcressBar:Connect(function(Content: string): () MissionModule.updateProgressBar(Content) end)
 
 GuiControl:init(Player)
 HudModule:init(Player)
